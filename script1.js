62f6c49361811dde9f1429b3572ca1ef32dd1ff3
saidusmon93 <73653661+saidusmon93@users.noreply.github.com>
2022-07-14 18:19:42 +0500

    reset

--- script1.js
+++ script1.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
         var tableAdd = xisob.map((item, i) => `
         <tr>
-                        <th scope="row">${i + 1}</th>
+                        <th scope="row">${i+1}</th>
                         <td>${item.izoh}</td>
                         <td>${item.summa}</td>
                         <td>${item.sana}  ${item.hour}</td>
@@ -19,38 +19,33 @@ $(document).ready(function () {
         `).join('')
         $('#text').html(tableAdd)
     }
-
-    if ($('#summa').val() < balance) {
-        $('#btn').click(() => {
-            var sana = new Date();
-            var yil = sana.toLocaleDateString();
-            var hour = sana.toLocaleTimeString();
-            var izoh = $('#izoh').val();
-            var summa = $('#summa').val();
-            var variant = $('#inputGroupSelect04').val();
-            var qiymat = summa * variant;
-            if (izoh == "" || summa == "") {
-                alert('Malumot kirgizing')
-            } else {
-                xisob.push({
-                    izoh: izoh,
-                    summa: Number(qiymat),
-                    sana: yil,
-                    hour: hour
-                });
-                $('#izoh').val('');
-                $('#summa').val('');
-            };
-            bal = xisob.reduce((total, index) => {
-                return total + index.summa
-            }, 0)
-            balance.text('Balans: ' + bal)
-            console.log(xisob)
-            addText();
-        });
-    }
-
-
+    $('#btn').click(() => {
+        var sana = new Date();
+        var yil = sana.toLocaleDateString();
+        var hour = sana.toLocaleTimeString();
+        var izoh = $('#izoh').val();
+        var summa = $('#summa').val();
+        var variant = $('#inputGroupSelect04').val();
+        var qiymat = summa * variant;
+        if (izoh == "" || summa == "") {
+            alert('Malumot kirgizing')
+        } else {
+            xisob.push({
+                izoh: izoh,
+                summa: Number(qiymat),
+                sana: yil,
+                hour: hour
+            });
+            $('#izoh').val('');
+            $('#summa').val('');
+        };
+        bal = xisob.reduce((total, index) => {
+            return total + index.summa
+        }, 0)
+        balance.text('Balans: ' + bal)
+        console.log(xisob)
+        addText();
+    });
     $('#kirim').click(() => {
         kirim = xisob.filter(kirim => {
             return kirim.summa > 0;
@@ -58,7 +53,7 @@ $(document).ready(function () {
 
         var tableAddKirim = kirim.map((item, i) => `
         <tr>
-                        <th scope="row">${i + 1}</th>
+                        <th scope="row">${i+1}</th>
                         <td>${item.izoh}</td>
                         <td>${item.summa}</td>
                         <td>${item.sana} ${item.hour}</td>
@@ -77,7 +72,7 @@ $(document).ready(function () {
         });
         var tableAddKirim = chiqim.map((item, i) => `
         <tr>
-                        <th scope="row">${i + 1}</th>
+                        <th scope="row">${i+1}</th>
                         <td>${item.izoh}</td>
                         <td>${item.summa}</td>
                         <td>${item.sana} ${item.hour}</td>
@@ -96,7 +91,7 @@ $(document).ready(function () {
         })
         var tableAddKirim = searchNote.map((item, i) => `
         <tr>
-                        <th scope="row">${i + 1}</th>
+                        <th scope="row">${i+1}</th>
                         <td>${item.izoh}</td>
                         <td>${item.summa}</td>
                         <td>${item.sana} ${item.hour}</td>
